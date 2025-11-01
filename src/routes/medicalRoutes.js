9cd0dc3c3c767476863f0b97c8a479c3633c7b6f
tasneem-ratrout <s12112159@stu.najah.edu>
2025-11-01 20:05:15 +0300

    update:Medication & Equipment Coordination

--- src/routes/medicalRoutes.js
+++ src/routes/medicalRoutes.js
@@ -15,23 +15,16 @@ const router = express.Router();
 router.use(authenticateUser);
 
 
-
-// 📦 إضافة دواء أو جهاز (Donor / NGO)
 router.post("/items", addMedicalItem);
 
-// 📋 عرض الأدوية والمعدات المتاحة (أي مستخدم)
 router.get("/items", getAvailableItems);
 
-// 🙋‍♀️ طلب مساعدة من المريض
 router.post("/aid", requestMedicalAid);
 
-// 🏢 NGO توافق أو ترفض الطلب
 router.put("/aid/:id/status", updateMedicalAidStatus);
 
-// 🚚 تحديث حالة التوصيل (NGO)
 router.put("/aid/:id/delivery", updateDeliveryStatus);
 
-// 👩‍⚕️ عرض طلبات المريض
 router.get("/aid/mypatient", getPatientRequests);
 
 
