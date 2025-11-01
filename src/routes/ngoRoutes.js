d8d057abb52cecfb6571eda6a43e591e9b6e801d
nareman joma <s12112343@stu.najah.edu>
2025-11-01 13:34:50 +0200

    Update ngoRoutes.js

--- src/routes/ngoRoutes.js
+++ src/routes/ngoRoutes.js
@@ -1,12 +1,12 @@
 // src/routes/ngoRoutes.js
 import express from 'express';
 import { requireAuth } from '../middleware/auth.js';
-import { addNGO ,verifyNGO} from '../controllers/ngoController.js'; // ✅ صح الآن
+import { addNGO ,verifyNGO} from '../controllers/ngoController.js'; 
 
 const router = express.Router();
 
-// ✅ مسار إضافة منظمة جديدة
 router.post('/add-ngo', requireAuth, addNGO);
 router.patch('/verify-ngo/:id', requireAuth, verifyNGO);
 
 export default router;
+
